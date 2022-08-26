df6ddeb4f151651e758cbb35d96844006e5e5650
Simon Lê <107181711+simonle251289@users.noreply.github.com>
2022-08-26 14:44:06 +0700

    Alo

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	})
+	for i := 0; i < 10000; i++ {
+		time.Sleep(5)
+		test()
+	}
+	err := http.ListenAndServe(":3001", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func test() {
 	resp, err := http.Get("https://www.google.com")
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Printf(err.Error())
 	}
-	log.Fatalln(resp)
+	fmt.Print(resp)
 }
